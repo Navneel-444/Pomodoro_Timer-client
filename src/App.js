7a442e311930d2a3c3b617122911108dc46cb4a1
Navneel Nandran <navneel.nandran@outlook.com>
2024-08-16 18:42:06 -0400

    build: navigation bar for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import NavBar from './components/NavBar/Navbar';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" ></Route>
         </Routes>
